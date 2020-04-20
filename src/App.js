60d9ef8d495c02635a4181a9a7918a42be4ff883
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-04-20 17:20:57 +1000

    barchart added and covid image added too

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import React, {Component} from 'react';
 import styles from './App.module.css';
 import { Cards, Chart, CountryPicker } from './components';
 import { fetchData } from './api';
+import coronaImage from './images/corona.png';
 
 class App extends Component {
   state = {
     data: {},
+    country: ''
   }
   async componentDidMount() {
     const fetchedData = await fetchData();
     //console.log(fetchedData);
     this.setState({data: fetchedData});
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData);
+    this.setState({data: fetchedData, country: country});
+  }
+
   render(){
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container} >
+          <img className = {styles.image} src={coronaImage} alt="corona" />
           <Cards data= {data}/>
-          <CountryPicker />
-          <Chart />
+          <CountryPicker handleCountryChange = {this.handleCountryChange} />
+          <Chart data={data} country={country} />
       </div>
     );
   }
