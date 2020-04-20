cbd66a6abd1bc9601765f2a3f258af661978f333
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-04-20 15:30:36 +1000

    api fetch with chart

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+export default function CountryPicker() {
+    return (
+        <div>
+            <h1> CountryPicker </h1>
+        </div>
+    )
+}
